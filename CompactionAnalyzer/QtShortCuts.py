02f6ba29e36e09115f864cc2335e5015406e35e1
David <david.boehringer@fau.de>
2021-07-28 09:28:13 +0200

    Update

--- CompactionAnalyzer/QtShortCuts.py
+++ CompactionAnalyzer/QtShortCuts.py
@@ -530,7 +530,7 @@ class EnterableLayout:
 
 
 class QVBoxLayout(QtWidgets.QVBoxLayout, EnterableLayout):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, no_margins=False):
         if parent is None and current_layout is not None:
             parent = current_layout
         if getattr(parent, "addLayout", None) is None:
@@ -539,10 +539,12 @@ class QVBoxLayout(QtWidgets.QVBoxLayout, EnterableLayout):
             super().__init__()
             parent.addLayout(self)
         self.layout = self
+        if no_margins is True:
+            self.setContentsMargins(0, 0, 0, 0)
 
 
 class QHBoxLayout(QtWidgets.QHBoxLayout, EnterableLayout):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, no_margins=False):
         if parent is None and current_layout is not None:
             parent = current_layout
         if getattr(parent, "addLayout", None) is None:#isinstance(parent, QtWidgets.QWidget):
@@ -551,6 +553,8 @@ class QHBoxLayout(QtWidgets.QHBoxLayout, EnterableLayout):
             super().__init__()
             parent.addLayout(self)
         self.layout = self
+        if no_margins is True:
+            self.setContentsMargins(0, 0, 0, 0)
 
 
 class QSplitter(QtWidgets.QSplitter, EnterableLayout):

158e31514309230687a5f169da3e7cd06145fa68
David <david.boehringer@fau.de>
2021-09-22 16:17:37 +0200

    Update parameter description

--- CompactionAnalyzer/CompactionFunctions.py
+++ CompactionAnalyzer/CompactionFunctions.py
@@ -206,25 +206,25 @@ def StuctureAnalysisMain(fiber_list,
                                                         # 7 um for collagen 
                          edge = 40   ,                  # Cutt of pixels at the edge since values at the border cannot be trusted
                          segmention_thres = 1.0 ,       # for cell segemetntion, thres 1 equals normal otsu threshold , user also can specify gaus1 + gaus2 in segmentation if needed
-                         seg_gaus1=0.5, seg_gaus2 = 100 ,  # 2 gaus filters used for local contrast enhancement; For seg_gaus2 = None a single gauss filter will be applied
-                         max_dist = None,                # optional: specify the maximal distance around cell center for analysis (in px)
-                         regional_max_correction = True,# correct background noise using regional maxima approach
-                         show_segmentation = False ,    # display the segmentation ooutput
-                         sigma_first_blur  = 0.5  ,     # slight first bluring of whole image before using structure tensor
+                         seg_gaus1=0.5, seg_gaus2 = 100 , # 2 gaus filters used as bandpassfilter for local contrast enhancement; For seg_gaus2 = None a single gauss filter is applied
+                         max_dist = None,               # optional: specify the maximal distance around cell center for analysis (in px)
+                         regional_max_correction = True,# background correction using regional maxima approach
+                         show_segmentation = False ,    # display the segmentation output
+                         sigma_first_blur  = 0.5  ,     # slight first bluring of whole image before appplying the structure tensor
                          angle_sections = 5    ,        # size of angle sections in degree 
                          shell_width =  None        ,   # pixel width of distance shells (px-value=um-value/scale)
-                         manual_segmention = False    , # manual segmentation of mask by click cell outline
-                         plotting = True     ,          # creates and saves plots additionally to excel files 
-                         dpi = 200      ,               # resolution of plots to be stored
-                         SaveNumpy = False       ,       # saves numpy arrays for later analysis - might create lots of data
-                         norm1=1,norm2 = 99  ,           # contrast spreading for input images by setting all values below norm1-percentile to zero and
-                                                        # all values above norm2-percentile to 1
-                         seg_invert=False,              # if segmentation is inverted dark objects are detected inseated of bright
-                         seg_iter = 1,                  # repetition of closing and dilation steps for segmentation
+                         manual_segmention = False    , # segmentation of mask by manual clicking the cell outline
+                         plotting = True     ,          # creates and saves individual figures additionally to the excel files 
+                         dpi = 200      ,               # resolution of figures 
+                         SaveNumpy = False       ,      # saves numpy arrays for later analysis - can create large data files
+                         norm1=1,norm2 = 99  ,          # contrast spreading for input images between norm1- and norm2-percentile; values below norm1-percentile are set to zero and
+                                                        # values above norm2-percentile are set to 1
+                         seg_invert=False,              # if segmentation is inverted dark objects are detected inseated of bright objects
+                         seg_iter = 1,                  # number of repetitions of  binary closing, dilation and filling holes steps
                          segmention_method="otsu",      #  use "otsu", "entropy" or "yen"  as segmentation method
-                         segmention_min_area = 1000,     
-                         load_segmentation = False,     # if true enter the path of the segementation math in path seg
-                         path_seg = None):
+                         segmention_min_area = 1000,    # small bjects below this px-area are removed during cell segmentation
+                         load_segmentation = False,     # if True enter the path of the segementation.npy - file in path_seg
+                         path_seg = None):              # to load a mask
     """
     Main analysis
     

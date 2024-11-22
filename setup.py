6f0ddbf86a966c4f6096e4d9b3869bca50c18427
David <david.boehringer@fau.de>
2024-11-22 11:49:49 +0100

    Adjust dependencies for newer version

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
-
 from setuptools import setup
 
 setup(
     name='CompactionAnalyzer',
     packages=['CompactionAnalyzer'],
-    version='1.0.2',
+    version='1.0.3',
     description='A Python package to analyze tissue compaction around cells in fiber materials',
     url='',
     download_url = '',
     author='David Böhringer, Andreas Bauer',
     author_email='david.boehringer@fau.de',
     license='GNU General Public License v3.0',
-    install_requires=['numpy>=1.16.2',
+    install_requires=['numpy>=1.2.6',
                       'pandas>=0.23.4',
                       'matplotlib>=2.2.2',
-                      'roipoly>=0.5.2',
+                      'roipoly>=0.5.3',
+					  'roipoly>=0.5.3',
+					  'scikit-image>=0.24.0',			  
 					  'pyyml',
+					  'tqdm',
+					  'openpyxl',
 					  'natsort',
 					  'matplotlib-scalebar'],
     keywords = ['structure', 'contractility', 'compaction','fibrosis', 'biophysics'],

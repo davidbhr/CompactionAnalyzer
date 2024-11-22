d56cd29dc6c8cacecdfc31763a4d693cf3bc667a
David <david.boehringer@fau.de>
2024-11-22 11:49:27 +0100

    Remove pyqt5 and Old GUI as we in future use the Saenopy GUI instead

--- CompactionAnalyzer/__init__.py
+++ CompactionAnalyzer/__init__.py
@@ -2,6 +2,6 @@ from . import StructureTensor
 from . import plotting
 from . import CompactionFunctions
 from . import utilities
-from . import GUI
-from . import QExtendedGraphicsView
-from . import QtShortCuts
+#from . import GUI     # now use the GUI from the saenopy package instead
+#from . import QExtendedGraphicsView
+#from . import QtShortCuts

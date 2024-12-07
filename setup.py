eaf7897c756b80bbd6d67b000a85468a8bf92abc
David Böhringer <davidboehringe@gmail.com>
2024-12-07 19:19:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='CompactionAnalyzer',
     packages=['CompactionAnalyzer'],
-    version='1.0.5',
+    version='1.0.6',
     description='A python package to analyze matrix fiber alignment around cells (proxy for cellular forces)',
     url='',
     download_url = '',
